9a36dce1ea47f7438836d0bbc34a429d000620ce
lljjjjjj <1523407063@qq.com>
2021-11-14 20:51:57 +0800

    init

---
+++ src/main/java/com/project/etsapi/EtSapiApplication.java
@@ -0,0 +1,13 @@
+package com.project.etsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EtSapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EtSapiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/etsapi/EtSapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.etsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EtSapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
