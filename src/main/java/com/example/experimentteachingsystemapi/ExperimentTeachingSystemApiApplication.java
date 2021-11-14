dd9b4e40fcc4dbdf68e24772460bb28297eed85d
lljjjjjj <1523407063@qq.com>
2021-11-14 20:28:07 +0800

    start

---
+++ src/main/java/com/example/experimentteachingsystemapi/ExperimentTeachingSystemApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.experimentteachingsystemapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExperimentTeachingSystemApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExperimentTeachingSystemApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/experimentteachingsystemapi/ExperimentTeachingSystemApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.experimentteachingsystemapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExperimentTeachingSystemApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
